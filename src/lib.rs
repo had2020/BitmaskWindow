ff96c2bf79137fe49d9d37c43757e000ceba1300
had2020 <hadrian.lazic@gmail.com>
2026-05-29 16:29:20 -0700

    working macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,20 @@
 
 #[macro_export]
 macro_rules! bitslice {
-    ($left_most_end:expr, $right_most_start:expr, $value:expr) => {{
-        let value = $value;
+    ($left_most_end:expr, $right_most_start:expr, $zero_of_value_type:expr, $value:expr) => {{
+        let bits = $zero_of_value_type;
         let width = $left_most_end - $right_most_start + 1;
-
-        let mask = (1 << width) - 1;
-
-        (value >> $right_most_start) & mask
+        let mask = bits ^ (width << $right_most_start);
+        !($value & mask)
     }};
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let test_value: u8 = 0b1111_1111;
-        let result: u8 = !bitslice!(4_u8, 2_u8, test_value);
-        assert_eq!(result, 0b0011_0000);
+        let test_value: u8 = 0b1011_0111;
+        let result: u8 = !bitslice!(4_u8, 2_u8, 0_u8, test_value);
+        assert_eq!(result, 0b0000_0100);
     }
 }
